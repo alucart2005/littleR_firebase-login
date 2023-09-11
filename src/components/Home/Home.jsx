760fd640036117404f3f447cb9253b3b2deb3a51
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-09-10 19:44:48 -0500

    3

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 
-export function Home() {
+export function Home(props) {
   return (
     <div>
       <div>
         <h1>
           <Link to="/login">Login</Link>
         </h1>
+        <br />
+        <h1>
+          <Link to="/signup">Signup</Link>
+        </h1>
       </div>
+      <h2>{props.name?`Welcome  ${props.name}`:"Start Session"}</h2>
     </div>
   );
 }
